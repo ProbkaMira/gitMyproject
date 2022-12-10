94d21b761a10c4aeff79d263ddb22ed6188a1bfb
ProbkaMira <gorvasskijaleksandr@gmail.com>
2022-12-10 19:43:11 +0200

    add .

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ $result = require __DIR__ . '/calc.php';
 <body>
 <form method="POST">
     <h1><u>Введите переменную:</u></h1>
-    <input class="a" type="text" name="x1" style="width: 150px; height: 20px;">
-    <select name="operation" style="height: 25px">
+    <input class="a" type="text" name="x1" >
+    <select class="d" name="operation">
         <option value="+">+</option>
         <option value="-">-</option>
         <option value="*">*</option>
         <option value="/">/</option>
     </select>
-    <input class="b" type="text" name="x2" style="width: 150px; height: 20px;">
-    <input class="c" type="submit" value="Посчитать" style="">
+    <input class="b" type="text" name="x2" >
+    <input class="c" type="submit" value="Посчитать" >
 </form>
 <p>Результат вычислений:</p>
     <?=$result ?>
